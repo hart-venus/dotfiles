9cb6ca4f147fd400d9d7b58618e5958849fb7779
Ariel Leyva <aleyva1509@gmail.com>
2024-06-07 01:41:21 -0600

    10 current  2024-06-07 01:34:17  24.05.803.b3b2b28c1daa  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -71,7 +67,6 @@
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -81,9 +76,9 @@
   users.users.hart = {
     isNormalUser = true;
     description = "Ariel Leyva";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -102,12 +97,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    libnotify
+    alejandra
     neovim
     neovide
     anytype
     git
   ];
-  
+
   environment.variables.EDITOR = "neovide --no-vsync";
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -129,7 +126,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -137,5 +133,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
