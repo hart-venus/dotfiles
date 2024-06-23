2a0e59259958e3f4ab92874d2509266507013ba5
Ariel Leyva <aleyva1509@gmail.com>
2024-06-23 13:47:52 -0600

    56 current  2024-06-23 13:47:49  24.05.20240606.9b5328b  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -207,6 +207,11 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  # Gnome Tweaks
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
