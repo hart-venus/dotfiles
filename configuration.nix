d33ebbb01b85e608003fc280c879ce5270fd2555
Ariel Leyva <aleyva1509@gmail.com>
2024-06-24 19:27:30 -0600

    59 current  2024-06-24 19:27:28  24.05.20240606.9b5328b  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -152,6 +152,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    qutebrowser
     cowsay
     lf
     godot_4
