77a3186b47a02e1725423b94d2c76bf1f7d9c591
Ariel Leyva <aleyva1509@gmail.com>
2024-08-04 17:24:43 -0600

    82 current  2024-08-04 17:24:29  24.05.20240606.9b5328b  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -157,6 +157,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    obsidian
     qutebrowser
     cowsay
     lf
