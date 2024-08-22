b7ce9e8038c17212dc9f7079dbe09c3a0e6879df
Ariel Leyva <aleyva1509@gmail.com>
2024-08-22 02:14:29 -0600

    103 current  2024-08-22 02:14:18  24.05.20240606.9b5328b  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -18,7 +18,7 @@
   networking.extraHosts = ''
     192.168.49.2 your-domain.com
     192.168.49.2 kibana.your-domain.com
-    192.168.49.2 ui.crazychess.com
+    192.168.49.2 ui.your-domain.com
   '';
 
   # Enable docker
