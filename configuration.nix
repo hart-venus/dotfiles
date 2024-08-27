00899701a40620ae52feeb2acb70d91e73dfd3c0
Ariel Leyva <aleyva1509@gmail.com>
2024-08-27 16:43:30 -0600

    110 current  2024-08-27 16:41:55  24.05.20240606.9b5328b  6.6.32                          *

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
